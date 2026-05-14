bd628ce8e071f29fa12eac123239e78466d96f45
Ryan Rey <ryanobanga243@gmail.com>
2026-05-14 23:29:48 +0300

    Delete commands/admin directory

--- commands/admin/antilink.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "antilink",
-    execute: async (conn, msg, args, { from, fancy, isOwner }) => {
-        if (!isOwner) return;
-        if (!args[0]) return msg.reply(fancy("ᴜꜱᴀɢᴇ: .ᴀɴᴛɪʟɪɴᴋ ᴏɴ/ᴏꜰꜰ"));
-        // Logic ipo kwenye handler.js tayari, hii ni toggle tu
-        msg.reply(fancy(`ᴀɴᴛɪʟɪɴᴋ ɪꜱ ɴᴏᴡ: ${args[0].toUpperCase()}`));
-    }
-};
