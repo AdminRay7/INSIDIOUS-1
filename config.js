3390c119a6525491395b138713d992d10309b02b
Ryan Rey <ryanobanga243@gmail.com>
2026-09-11 00:52:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,25 @@
 module.exports = {
     // Bot Info
     botName: "INSIDIOUS",
-    ownerNumber: "254794376595", // Your WhatsApp number
+    ownerNumber: "254794376595",
     ownerName: "StanyTZ",
     version: "2.1.1",
     footer: "INSIDIOUS V2",
     prefix: ".",
-    
+
     // Session
     sessionName: "insidious_session",
-    
-    // Database - Hardcoded with your credentials
-    mongodb: "mongodb+srv://admin:ryan.3063@cluster0.iylgadk.mongodb.net/insidious?retryWrites=true&w=majority",
-    
+
+    // Database
+    mongodb: "mongodb+srv://presenter:ryan.3063@cluster0.94sivgt.mongodb.net/?appName=Cluster0",
+
     // AI
     aiModel: "https://ai.servietsky1.workers.chat/?message=",
-    
-    // Channel (optional - leave empty if you don't have)
+
+    // Channel (optional)
     newsletterJid: "",
     channelLink: "",
-    
+
     // Features
     autoRead: true,
     autoReact: true,
@@ -35,26 +35,22 @@ module.exports = {
     antitag: false,
     antimedia: "off",
     workMode: "public",
-    
+
     // Auto Status
     autoStatus: {
         view: true,
         like: true,
         reply: false
     },
-    
-    // Autoblock (empty = no blocking)
+
     autoblock: [],
-    
-    // Warning words
+
     scamWords: ["free money", "lottery", "winner", "cash prize"],
     pornWords: ["porn", "xxx", "adult", "nude", "sex"],
-    
-    // Sleep mode (optional)
+
     sleepStart: "23:00",
     sleepEnd: "06:00",
     groupJid: "",
-    
-    // Menu image
+
     menuImage: "https://telegra.ph/file/5f6b3c7a8d9e0f1a2b3c.jpg"
 };
