20ca348196719f04bf532bfb2de865475a41c3b9
Ryan Rey <ryanobanga243@gmail.com>
2026-09-12 01:34:55 +0300

    Create manifest.js

---
+++ public/manifest.js
@@ -0,0 +1,11 @@
+{
+  "name": "INSIDIOUS Mobile",
+  "short_name": "INSIDIOUS",
+  "start_url": "/mobile",
+  "display": "standalone",
+  "background_color": "#0a0a0a",
+  "theme_color": "#ff3366",
+  "icons": [
+    { "src": "/Assets/menu.jpg", "sizes": "512x512", "type": "image/jpeg" }
+  ]
+}
